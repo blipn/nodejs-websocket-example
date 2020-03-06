a50c62e1f2f9d28488db7d38e8c6ccf27cd9a8f2
Quentin <oblipn@gmail.com>
2020-03-06 09:49:27 +0100

    add ngrok

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+const ngrok = require('ngrok');
+
+(async function() {
+  const url = await ngrok.connect(port);
+  console.log(`Forwarding localhost:${port} on ${url}`)
+})();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// msg: {
+//   hashTag: String,
+//   from: String,
+//   message: String
+// }
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('message', function(msg){
+    io.emit(msg.hashTag, msg);
   });
 });
 
