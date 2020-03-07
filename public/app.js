5d1064c24517f22e714a6abef990f66d65244a2c
Quentin <oblipn@gmail.com>
2020-03-07 15:28:15 +0100

    install prompt

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
+
+/* Put code here */
+
+/* Only register a service worker if it's supported */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceWorker.js');
+}
