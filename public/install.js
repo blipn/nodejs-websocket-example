5b6986fee83b84551ea5ec588a3f9a2182ffe222
Quentin <oblipn@gmail.com>
2020-04-09 15:17:45 +0200

    fix install

--- public/install.js
+++ public/install.js
@@ -1,35 +1,61 @@
 let deferredPrompt;
 let buttonInstall = document.getElementById("install")
-
-function showInstallPromotion() {
-  buttonInstall.style.filter = "brightness(1)"
-  console.log("install promotion on")
-  buttonInstall.addEventListener('click', (e) => {
-    // Hide the app provided install promotion
-    hideMyInstallPromotion();
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-    })
-  });
-}
-
-function hideMyInstallPromotion() {
-  buttonInstall.style.filter = "brightness(0.5)"
-}
+buttonInstall.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+  // Update UI to notify the user they can add to home screen
+  buttonInstall.style.display = 'block';
+
+  buttonInstall.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    buttonInstall.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
 });
+
+// function showInstallPromotion() {
+//   buttonInstall.style.filter = "brightness(1)"
+//   console.log("install promotion on")
+//   buttonInstall.addEventListener('click', (e) => {
+//     // Hide the app provided install promotion
+//     hideMyInstallPromotion();
+//     // Show the install prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === 'accepted') {
+//         console.log('User accepted the install prompt');
+//       } else {
+//         console.log('User dismissed the install prompt');
+//       }
+//     })
+//   });
+// }
+
+// function hideMyInstallPromotion() {
+//   buttonInstall.style.filter = "brightness(0.5)"
+// }
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent the mini-infobar from appearing on mobile
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can install the PWA
+//   showInstallPromotion();
+// });
 
